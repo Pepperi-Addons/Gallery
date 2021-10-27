2582a8525b49ed6131ee0743604919b1737cf63c
Avner Eisenberg <avner.e@pepperi.com>
2021-10-27 15:17:46 +0300

    start working on the gallery editor

--- client-side/src/app/block-editor/gallery-editor.component.ts
+++ client-side/src/app/block-editor/gallery-editor.component.ts
@@ -1,24 +1,90 @@
 import { TranslateService } from '@ngx-translate/core';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { IGallery, IGalleryEditor, IHostObject, IImageEditor } from '../gallery.model';
 
 @Component({
     selector: 'gallery-editor',
     templateUrl: './gallery-editor.component.html',
-    styleUrls: ['./gallery-editor.component.css']
+    styleUrls: ['./gallery-editor.component.scss']
 })
 export class GalleryEditorComponent implements OnInit {
-    @Input() hostObject: any;
+    // @Input() hostObject: any;
 
     @Output() hostEvents: EventEmitter<any> = new EventEmitter<any>();
 
+    @Input()
+    set hostObject(value: IHostObject) {
+        if (value && value.configuration) {
+            this._configuration = value.configuration
+        } else {
+            // TODO - NEED TO ADD DEFAULT SLIDE
+            this.loadDefaultConfiguration();
+        }
+    }
+    
+    private _configuration: IGallery;
+    get configuration(): IGallery {
+        return this._configuration;
+    }
+
     constructor(private translate: TranslateService) { }
 
-    ngOnInit(): void {
+    async ngOnInit(): Promise<void> {
+        if (!this.configuration) {
+            this.loadDefaultConfiguration();
+        }
+        const desktopTitle = await this.translate.get('SLIDESHOW.HEIGHTUNITS_REM').toPromise();
+
+        // this.buttonColor = [
+        //     { key: 'system-primary', value:this.translate.instant('SLIDE_EDITOR.BUTTON_COLOR.SYSTEM') },
+        //     { key: 'invert', value:this.translate.instant('SLIDE_EDITOR.BUTTON_COLOR.INVERTED') },
+        //     { key: 'user-primary', value:this.translate.instant('SLIDE_EDITOR.BUTTON_COLOR.USER') },
+        // ]
+
+
         // When finish load raise block-editor-loaded.
         this.hostEvents.emit({action: 'block-editor-loaded'});
     }
 
     ngOnChanges(e: any): void {
         
+    }
+
+    private updateHostObject() {
+        
+        this.hostEvents.emit({
+            action: 'set-configuration',
+            configuration: this.configuration
+        });
+    }
+
+    onGalleryFieldChange(key, event){
+        const value = event && event.source && event.source.key ? event.source.key : event && event.source && event.source.value ? event.source.value :  event;
+        debugger;
+        if(key.indexOf('.') > -1){
+            let keyObj = key.split('.');
+            this.configuration.galleryConfig[keyObj[0]][keyObj[1]] = value;
+        }
+        else{
+            this.configuration.galleryConfig[key] = value;
+        }
+
+        this.updateHostObject();
+    }
+
+    private loadDefaultConfiguration() {
+        this._configuration = this.getDefaultHostObject();
+        //this.updateHostObject();
+    }
+
+    private getDefaultImage(): IImageEditor {
+        let image = new IImageEditor();
+        //a.id = 0;
+
+        return image;
+    }
+
+    private getDefaultHostObject(): IGallery {
+        return { galleryConfig: new IGalleryEditor(), images: [this.getDefaultImage()] };
     }
 }
--- client-side/src/app/block-editor/gallery-editor.module.ts
+++ client-side/src/app/block-editor/gallery-editor.module.ts
@@ -2,15 +2,56 @@ import { TranslateLoader, TranslateModule, TranslateService, TranslateStore } fr
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { PepAddonService, PepFileService } from '@pepperi-addons/ngx-lib';
-
 import { GalleryEditorComponent } from './gallery-editor.component';
+import { PepAddonService, PepCustomizationService, PepFileService, PepHttpService, PepNgxLibModule } from '@pepperi-addons/ngx-lib';
+import { MatTabsModule } from '@angular/material/tabs';
+import { PepButtonModule } from '@pepperi-addons/ngx-lib/button';
+import { PepTextboxModule } from '@pepperi-addons/ngx-lib/textbox';
+import { PepCheckboxModule } from '@pepperi-addons/ngx-lib/checkbox';
+import { PepSelectModule } from '@pepperi-addons/ngx-lib/select';
+import { PepGroupButtonsModule } from '@pepperi-addons/ngx-lib/group-buttons';
+import { PepColorModule } from '@pepperi-addons/ngx-lib/color';
+import { PepImageModule } from '@pepperi-addons/ngx-lib/image';
+import { PepTextareaModule, } from '@pepperi-addons/ngx-lib/textarea';
+import { PepPageLayoutModule } from '@pepperi-addons/ngx-lib/page-layout';
+import { MatSliderModule } from '@angular/material/slider';
+import { pepIconTextAlignCenter, pepIconTextAlignLeft, pepIconTextAlignRight, pepIconArrowBackRight, pepIconArrowBackLeft, pepIconArrowBack, pepIconArrowLeftAlt,pepIconArrowDown, pepIconArrowUp, PepIconModule, pepIconNumberPlus, PepIconRegistry, pepIconSystemBin, pepIconSystemBolt, pepIconSystemClose, pepIconSystemEdit, pepIconSystemMove } from '@pepperi-addons/ngx-lib/icon';
+import { PepDialogService } from '@pepperi-addons/ngx-lib/dialog';
 
 import { config } from '../addon.config';
+
+const pepIcons = [
+    pepIconTextAlignCenter, 
+    pepIconTextAlignLeft, 
+    pepIconTextAlignRight, 
+    pepIconArrowBackRight, 
+    pepIconArrowBackLeft,
+    pepIconArrowBack,
+    pepIconSystemClose,
+    pepIconNumberPlus,
+    pepIconSystemBolt,
+    pepIconSystemEdit,
+    pepIconSystemMove,
+    pepIconSystemBin,
+    pepIconArrowLeftAlt,
+    pepIconArrowDown,
+    pepIconArrowUp
+];
 
 @NgModule({
     declarations: [GalleryEditorComponent],
     imports: [
+        PepButtonModule,
+        PepTextboxModule,
+        PepSelectModule,
+        PepCheckboxModule,
+        PepPageLayoutModule,
+        PepGroupButtonsModule,
+        MatTabsModule,
+        PepColorModule,
+        PepImageModule,
+        PepTextareaModule,
+        MatSliderModule,
         CommonModule,
         TranslateModule.forChild({
             loader: {
@@ -23,7 +64,13 @@ import { config } from '../addon.config';
     ],
     exports: [GalleryEditorComponent],
     providers: [
+        HttpClient,
         TranslateStore,
+        PepHttpService,
+        PepAddonService,
+        PepFileService,
+        PepCustomizationService,
+        PepDialogService
         // Add here all used services.
     ]
 })
